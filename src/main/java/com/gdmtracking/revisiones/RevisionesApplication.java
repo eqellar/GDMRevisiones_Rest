0c1cf2713f2785c37393966a4758954ad0c8d07c
ecuellar <eqellar@gmail.com>
2021-12-01 20:01:26 -0500

    Inicio con conexión a BD mySql

---
+++ src/main/java/com/gdmtracking/revisiones/RevisionesApplication.java
@@ -0,0 +1,13 @@
+package com.gdmtracking.revisiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdmtracking/revisiones/RevisionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdmtracking.revisiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
